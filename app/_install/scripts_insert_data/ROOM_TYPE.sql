4b47cd471341072286c524ec668e535950aeb68f
TeonaRusu <rusuteona@gmail.com>
2018-12-03 10:47:40 +0200

    add insert data scripts

---
+++ app/_install/scripts_insert_data/ROOM_TYPE.sql
@@ -0,0 +1,11 @@
+INSERT INTO ROOM_TYPE (type_id, room_type, room_price, room_details) VALUES
+('1', 'Sigle', 557, 'A room assigned to one person. May have one or more beds.'),
+('2', 'Double', 146, 'A room assigned to two people. May have one or more beds.'),
+('3', 'Triple', 303, 'A room assigned to three people. May have two or more beds.'),
+('4', 'Quad', 939, 'A room assigned to four people. May have two or more beds.'),
+('5', 'Queen', 385, 'A room with a queen-sized bed. May be occupied by one or more people.'),
+('6', 'Twin', 330, 'A room with two beds. May be occupied by one or more people.'),
+('7', 'Double-double', 110, 'A room with two double (or perhaps queen) beds. May be occupied by one or more people.'),
+('8', 'Studio', 597, 'A room with a studio bed – a couch that can be converted into a bed. May also have an additional bed.'),
+('9', 'Master Suite', 841, 'A parlour or living room connected to one or more bedrooms.'),
+('10','Junior Suite', 734, 'A single room with a bed and sitting area. Sometimes the sleeping area is in a bedroom separate from the parlour or living room.');

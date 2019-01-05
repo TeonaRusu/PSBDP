69c5d1695a169f9e9fbdb96ed5dbb265e9b92c51
TeonaRusu <rusuteona@gmail.com>
2019-01-05 18:46:13 +0200

    edit_reservation

--- application/Model/Reservation.php
+++ application/Model/Reservation.php
@@ -36,17 +36,16 @@ class Reservation extends Model {
       return $reservations;
     }
 
-    function get_reservation_details($reservation_id)
+	function get_reservation_details($reservation_id)
     {
       $reservation_details = array();
       $temp = array('' => '');
-      $conn = oci_connect('c##psbdp', 'psbdp', 'localhost:1521/orcl.local');  /* Roxana */
-      // $conn = oci_connect('c##teona', 'teona', 'localhost:1521/orcl');    /* Teona */
+      //$conn = oci_connect('c##psbdp', 'psbdp', 'localhost:1521/orcl.local');  /* Roxana */
+      $conn = oci_connect('c##teona', 'teona', 'localhost:1521/orcl');    /* Teona */
         if (!$conn) {
             $e = oci_error();
             trigger_error(htmlentities($e['message']), E_USER_ERROR);
         }
-
       $curs = oci_new_cursor($conn);
       $stid = oci_parse($conn, "begin GET_RESERVATION_DETAILS(:id, :cursbv); end;");
       oci_bind_by_name($stid, ":cursbv", $curs, -1, OCI_B_CURSOR);
@@ -54,10 +53,10 @@ class Reservation extends Model {
       oci_execute($stid);
       oci_execute($curs);  // Execute the REF CURSOR like a normal statement id
       while (($row = oci_fetch_array($curs, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
-        $temp = array('RSV_ID' => $row['RSV_ID'], 'CHECKIN_DATE' => $row['CHECKIN_DATE'], 'CHECKOUT_DATE' => $row['CHECKOUT_DATE'],'RSV_DATE' => $row['RSV_DATE'],
+        $reservation_details = array('RSV_ID' => $row['RSV_ID'], 'CHECKIN_DATE' => $row['CHECKIN_DATE'], 'CHECKOUT_DATE' => $row['CHECKOUT_DATE'],'RSV_DATE' => $row['RSV_DATE'],
                     'RSV_STATUS' => $row['RSV_STATUS'], 'RSV_PRICE' => $row['RSV_PRICE'], 'CUSTOMER_ID' => $row['CUSTOMER_ID'], 'EMP_ID' => $row['EMP_ID'],
           'ROOM_NUMBER' => $row['ROOM_NUMBER']);
-        array_push($reservation_details, $temp);
+        break;
       }
       // print_r ($reservation_details);
       oci_free_statement($stid);
@@ -67,32 +66,54 @@ class Reservation extends Model {
       return $reservation_details;
     }
 
-    function update_reservation($rsv_id, $checkin, $checkout, $date, $price, $status, $cust_id, $emp_id, $room_nr)
+
+function update_reservation($rsv_id, $checkin, $checkout, $date, $price, $status, $emp_id, $room_nr)
     {
-      $conn = oci_connect('c##psbdp', 'psbdp', 'localhost:1521/orcl.local');  /* Roxana */
-      // $conn = oci_connect('c##teona', 'teona', 'localhost:1521/orcl');    /* Teona */
+      //$conn = oci_connect('c##psbdp', 'psbdp', 'localhost:1521/orcl.local');  /* Roxana */
+      $conn = oci_connect('c##teona', 'teona', 'localhost:1521/orcl');    /* Teona */
       if (!$conn) {
           $e = oci_error();
           trigger_error(htmlentities($e['message']), E_USER_ERROR);
         }
-      echo 'Pe aici';
-      $stid = oci_parse($conn, "begin UPDATE_RESERVATION(:rsvid, :checkin, :checkout, :date, :price, :status, :cust_id, :emp_id, :room_nr); end;");
+      
+      $stid = oci_parse($conn, "begin UPDATE_RESERVATION(:rsvid, :checkin, :checkout, :date, :price, :status, :emp_id, :room_nr); end;");
       oci_bind_by_name($stid, ":rsvid", $rsv_id, 255);
       oci_bind_by_name($stid, ":checkin", $checkin);
       oci_bind_by_name($stid, ":checkout", $checkout);
       oci_bind_by_name($stid, ":date", $date);
-      oci_bind_by_name($stid, ":price", $price);
+      oci_bind_by_name($stid, ":price", $price, 255);
       oci_bind_by_name($stid, ":status", $status);
-      oci_bind_by_name($stid, ":cust_id", $cust_id);
-      oci_bind_by_name($stid, ":emp_id", $emp_id);
-      oci_bind_by_name($stid, ":room_nr", $room_nr);
-
+      //oci_bind_by_name($stid, ":cust_id", $cust_id, 255);
+      oci_bind_by_name($stid, ":emp_id", $emp_id, 255);
+      oci_bind_by_name($stid, ":room_nr", $room_nr, 255);
       oci_execute($stid, OCI_COMMIT_ON_SUCCESS);
-      echo 'Done';
       oci_free_statement($stid);
       oci_close($conn);
     }
+}
 
+function add_reservation($checkin, $checkout, $date, $price, $status, $cust_id, $emp_id, $room_nr)
+    {
+      //$conn = oci_connect('c##psbdp', 'psbdp', 'localhost:1521/orcl.local');  /* Roxana */
+      $conn = oci_connect('c##teona', 'teona', 'localhost:1521/orcl');    /* Teona */
+      if (!$conn) {
+          $e = oci_error();
+          trigger_error(htmlentities($e['message']), E_USER_ERROR);
+        }
+      
+      $stid = oci_parse($conn, "begin ADD_RESERVATION(:checkin, :checkout, :date, :price, :status, :cust_id, :emp_id, :room_nr); end;");
+      oci_bind_by_name($stid, ":rsvid", $rsv_id, 255);
+      oci_bind_by_name($stid, ":checkin", $checkin);
+      oci_bind_by_name($stid, ":checkout", $checkout);
+      oci_bind_by_name($stid, ":date", $date);
+      oci_bind_by_name($stid, ":price", $price, 255);
+      oci_bind_by_name($stid, ":status", $status);
+      oci_bind_by_name($stid, ":cust_id", $cust_id, 255);
+      oci_bind_by_name($stid, ":emp_id", $emp_id, 255);
+      oci_bind_by_name($stid, ":room_nr", $room_nr, 255);
+      oci_execute($stid, OCI_COMMIT_ON_SUCCESS);
+      oci_free_statement($stid);
+      oci_close($conn);
 }
 
 ?>
--- application/view/reservation/edit.php
+++ application/view/reservation/edit.php
@@ -1,12 +1,14 @@
 <style>
 .header-sec{
     background: #346CE9;
+   /* min-height: 40px;*/
+    border-radius: 5px;
     color: white;
     text-align: center;
     vertical-align: baseline;
     font: inherit;
     display: block;
-    margin-top: 0px;
+    margin-top: 0;
     padding: 1px;
     border: 0;
     outline: 0;
@@ -30,89 +32,65 @@
     box-sizing: border-box;
 }
 
+a.button {
+    -webkit-appearance: button;
+    -moz-appearance: button;
+    appearance: button;
+
+    text-decoration: none;
+    color: initial;
+}
+
 </style>
 <div class="date-form">
   <div class="header-sec">
     <h3> Reservation details</h3>
   </div>
-  <!-- <form name="editReservation" action="<?php //echo URL; ?>reservation/editReservation" method="post" role="form"> -->
-  <form name="editReservation" method="post" role="form">
+  <form method="POST" action="#" >
     <div style="width: 50%; float:left; background-color:#fefefe; height: 400px; padding-left: 15px;">
         <div class="form-row">
           <label>ID</label>
-          <input id="rsv_id" class="form-control" type="text" name="rsv_id" required />
+          <input id="rsv_id" class="form-control" type="text" name="RSV_ID" value="<?php echo $reservation_details['RSV_ID']; ?>" required />
         </div>
         <div class="form-row">
           <label>Checkin-date</label>
-          <input id="checkin_date" class="form-control" type="text" name="checkin_date" required />
+          <input id="checkin_date" class="form-control" type="text" name="CHECKIN_DATE" value="<?php echo $reservation_details['CHECKIN_DATE']; ?>" required />
         </div>
-        <div class="form-row">
+		<div class="form-row">
           <label>Checkout-date</label>
-          <input id="checkout_date" class="form-control" type="text" name="checkout_date" required />
+          <input id="checkout_date" class="form-control" type="text" name="CHECKOUT_DATE" value="<?php echo $reservation_details['CHECKOUT_DATE']; ?>" required />
         </div>
+		
         <div class="form-row">
           <label>Reservation day</label>
-          <input id="rsv_date" class="form-control" type="text" name="rsv_date" required />
+          <input id="rsv_date" class="form-control" type="text" name="RSV_DATE" value="<?php echo $reservation_details['RSV_DATE']; ?>" required />
         </div>
         <div class="form-row">
           <label>Price</label>
-          <input id="rsv_price" class="form-control" type="text" name="rsv_price" required />
+          <input id="rsv_price" class="form-control" type="text" name="RSV_PRICE" value="<?php echo $reservation_details['RSV_PRICE']; ?>" required />
         </div>
     </div>
+
      <div style="width: 50%; float:right; background-color:#fefefe; height: 400px; padding-left: 15px">
        <div class="form-row">
           <label>Status</label>
-          <input id="rsv_status" class="form-control" type="text" name="status" required />
+          <input id="rsv_status" class="form-control" type="text" name="RSV_STATUS" value="<?php echo $reservation_details['RSV_STATUS']; ?>" required />
         </div>
         <div class="form-row">
           <label>Customer #ID</label>
-            <input id="customer_id" class="form-control" type="text" name="customer_id" required />
+            <input id="customer_id" class="form-control" type="text" name="CUSTOMER_ID" disabled value="<?php echo $reservation_details['CUSTOMER_ID']; ?>" required />
         </div>
         <div class="form-row">
           <label>Emp #ID</label>
-            <input id="emp_id" class="form-control" type="text" name="emp_id" required />
+            <input id="emp_id" class="form-control" type="text" name="EMP_ID" value="<?php echo $reservation_details['EMP_ID']; ?>" required />
         </div>
         <div class="form-row">
-          <label>Room #ID</label>
-          <input id="room_number" class="form-control" type="text" name="room_number" required />
+          <label>Room Number</label>
+          <input id="room_id" class="form-control" type="text" name="ROOM_NUMBER" value="<?php echo $reservation_details['ROOM_NUMBER']; ?>" required />
         </div>
     </div>
-    <input type="submit">
+    <input type="submit" name="SAVE" value="Save"/>
+	<a href="<?php echo URL; ?>reservation" class="button">Close Edit</a>
   </form>
   <span id="msg-edit"></span>
 </div>
-<?php 
-    //old values
-    foreach ($reservation_array as $r) {
-        $id = $r['RSV_ID'];
-        $checkin_date = $r['CHECKIN_DATE'];
-        $checkout_date = $r['CHECKOUT_DATE'];
-        $rsv_date =  $r['RSV_DATE'];
-        $rsv_price = $r['RSV_PRICE'];
-        $rsv_status = $r['RSV_STATUS'];
-        $cust_id = $r['CUSTOMER_ID']; 
-        $emp_id = $r['EMP_ID'];
-        $room_number = $r['ROOM_NUMBER']; 
-    }
-?>
-<script type="text/javascript">
-    var id = "<?php echo $id ?>";
-    var checkin_date = "<?php echo $checkin_date ?>";
-    var checkout_date = "<?php echo $checkout_date ?>";
-    var rsv_date = "<?php echo $rsv_date ?>";
-    var rsv_price = "<?php echo $rsv_price ?>";
-    var rsv_status = "<?php echo $rsv_status ?>";
-    var cust_id = "<?php echo $cust_id ?>";
-    var emp_id = "<?php echo $emp_id ?>";
-    var room_number = "<?php echo $room_number ?>";
-
-    document.forms['editReservation'].elements['rsv_id'].value = id;
-    document.forms['editReservation'].elements['checkin_date'].value = checkin_date;
-    document.forms['editReservation'].elements['checkout_date'].value = checkout_date;
-    document.forms['editReservation'].elements['rsv_date'].value = rsv_date;
-    document.forms['editReservation'].elements['rsv_price'].value = rsv_price;
-    document.forms['editReservation'].elements['rsv_status'].value = rsv_status;
-    document.forms['editReservation'].elements['customer_id'].value = cust_id;
-    document.forms['editReservation'].elements['emp_id'].value = emp_id;
-    document.forms['editReservation'].elements['room_number'].value = room_number;
-</script>
